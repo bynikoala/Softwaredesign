a8676a1a44de621b737cbf34667bcfec75ad12c4
bynikoala <34539683+bynikoala@users.noreply.github.com>
2018-10-29 13:09:59 +0100

    created A02

---
+++ A02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 42;
+            var pi = 3.141592f;
+            var salute = "Hellouw ♥";
+            
+            Console.WriteLine(i.GetType());
+            Console.WriteLine(pi.GetType());
+            Console.WriteLine(salute.GetType());
+        }
+    }
+}
