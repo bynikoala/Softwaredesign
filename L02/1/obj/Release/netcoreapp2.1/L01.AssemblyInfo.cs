1a8d35de28369647442f9f972935d1c73e849ef8
bynikoala <34539683+bynikoala@users.noreply.github.com>
2018-11-05 17:14:06 +0100

    references

--- L02/1/obj/Release/netcoreapp2.1/L01.AssemblyInfo.cs
+++ L02/1/obj/Release/netcoreapp2.1/L01.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("A02")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("L01")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("A02")]
-[assembly: System.Reflection.AssemblyTitleAttribute("A02")]
+[assembly: System.Reflection.AssemblyProductAttribute("L01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("L01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
