b79f5838591a149aa2016191a7ffce5ce4877ea5
bynikoala <34539683+bynikoala@users.noreply.github.com>
2018-12-05 09:51:01 +0100

    restore + new A07 - Chiara

---
+++ A07/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace A07 {
+    class Program {
+        static int score = 0;
+        private static int i = 0;
+        static void Main (string[] args) {
+            
+        }
+    }
+}
