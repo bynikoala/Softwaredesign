3f5c5d9d1c70a6c32487f62b4a3dc86ac23e9e71
bynikoala <34539683+bynikoala@users.noreply.github.com>
2019-01-07 23:43:42 +0100

    a10 vom notebook

---
+++ A10/Tree.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace A10 {
+    class Tree<T> {
+        public T treeVar;
+        public List<Tree<T>> children = new List<Tree<T>> ();
+        public Tree<T> parent;
+        public Tree<T> CreateNode (T nodeVar) {
+            Tree<T> nodeTree = new Tree<T> {
+                treeVar = nodeVar
+            };
+            return nodeTree;
+        }
+        public void AppendChild (Tree<T> childNode) {
+            children.Add (childNode);
+            childNode.parent = this;
+        }
+        public void RemoveChild (Tree<T> childNode) {
+            children.Remove (childNode);
+        }
+        public void PrintTree () {
+            Console.WriteLine (treeVar);
+            if (children.Count > 0) {
+                foreach (Tree<T> child in children) {
+                    Console.Write ("*");
+                    if (parent != null)
+                        Console.Write ("*");
+                    child.PrintTree ();
+                }
+            }
+        }
+        public void PrintAncestors () {
+            if (parent != null) {
+                parent.PrintAncestors ();
+            }
+            Console.Write("#");
+            Console.WriteLine (treeVar);
+        }
+    }
+}
