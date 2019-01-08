e791b7c2119720c74c61eb49ca5fb3552ddcf3ef
bynikoala <34539683+bynikoala@users.noreply.github.com>
2019-01-08 09:56:18 +0100

    formatting

--- A10/Tree.cs
+++ A10/Tree.cs
@@ -4,38 +4,38 @@ using System.Collections.Generic;
 namespace A10 {
     class Tree<T> {
         public T treeVar;
-        public List<Tree<T>> children = new List<Tree<T>> ();
+        public List<Tree<T>> children = new List<Tree<T>>();
         public Tree<T> parent;
-        public Tree<T> CreateNode (T nodeVar) {
+        public Tree<T> CreateNode(T nodeVar) {
             Tree<T> nodeTree = new Tree<T> {
                 treeVar = nodeVar
             };
             return nodeTree;
         }
-        public void AppendChild (Tree<T> childNode) {
-            children.Add (childNode);
+        public void AppendChild(Tree<T> childNode) {
+            children.Add(childNode);
             childNode.parent = this;
         }
-        public void RemoveChild (Tree<T> childNode) {
-            children.Remove (childNode);
+        public void RemoveChild(Tree<T> childNode) {
+            children.Remove(childNode);
         }
-        public void PrintTree () {
-            Console.WriteLine (treeVar);
+        public void PrintTree() {
+            Console.WriteLine(treeVar);
             if (children.Count > 0) {
                 foreach (Tree<T> child in children) {
-                    Console.Write ("*");
+                    Console.Write("*");
                     if (parent != null)
-                        Console.Write ("*");
-                    child.PrintTree ();
+                        Console.Write("*");
+                    child.PrintTree();
                 }
             }
         }
-        public void PrintAncestors () {
+        public void PrintAncestors() {
             if (parent != null) {
-                parent.PrintAncestors ();
+                parent.PrintAncestors();
             }
             Console.Write("#");
-            Console.WriteLine (treeVar);
+            Console.WriteLine(treeVar);
         }
     }
 }
